6051358d32ecd983268c07e14ca3343ee3be27cc
stefanbutuc <butucstefan@gmail.com>
2020-04-12 17:13:43 +0100

    Updates

---
+++ precache-manifest.4b69b6927718f9e913da9e35f73608cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497cc91fb8ee6b832d6a54e118a1dca0",
+    "url": "/butuchat/index.html"
+  },
+  {
+    "revision": "2fbca580e35edcca7dc6",
+    "url": "/butuchat/static/css/main.346e8421.chunk.css"
+  },
+  {
+    "revision": "4909cf8e2168e3caa944",
+    "url": "/butuchat/static/js/2.3feda4b4.chunk.js"
+  },
+  {
+    "revision": "2fbca580e35edcca7dc6",
+    "url": "/butuchat/static/js/main.47c98caa.chunk.js"
+  },
+  {
+    "revision": "62e9e304e739f8227af0",
+    "url": "/butuchat/static/js/runtime~main.bc92a3ed.js"
+  }
+]);
